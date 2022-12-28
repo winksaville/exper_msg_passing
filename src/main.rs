26caeac37395983886708a62da2cbde26ab6c5b4
Wink Saville <wink@saville.com>
2022-12-27 16:59:58 -0800

    feat: Use Arc<RwLoc<Option<SenderWrapper<Box<SuperProtovcol>>>>>
    Going to use a channel instaead.

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,86 @@
+use std::{thread, sync::{mpsc::{Sender, channel, Receiver}, RwLock, Arc}};
+
 use exper_msg_passing::{Client, Pinger, Server, ServiceManager, StartMsg, SuperProtocol};
 
+pub struct SenderWrapper<T>(Sender<T>);
+
+//impl<T> SenderWrapper<T> {
+//    fn new(sender: Sender<T>) -> Self {
+//        Self(sender)
+//    }
+//}
+unsafe impl<T> Send for SenderWrapper<T> {}
+unsafe impl<T> Sync for SenderWrapper<T> {}
+
 fn main() {
-    let client = Client::default();
-    let server = Server::default();
+    //let client = Client::default();
+    //let mut client_service_manager = ServiceManager::default();
+    //client_service_manager.register_service(Box::new(client));
 
-    let mut service_manager = ServiceManager::default();
-    service_manager.register_service(Box::new(client));
-    service_manager.register_service(Box::new(server));
+    //let server = Server::default();
+    //let mut server_service_manager = ServiceManager::default();
+    //server_service_manager.register_service(Box::new(client));
 
-    let client_tx = service_manager.get_sender(0);
-    let server_tx = service_manager.get_sender(1);
+    //let client_tx = client_service_manager.get_sender(0);
+    //let server_tx = server_service_manager.get_sender(0);
 
-    for _ in 0..2 {
-        let msg = Box::new(SuperProtocol::P2(Pinger::Start(StartMsg {
-            count: 1,
-            client_tx: Some(client_tx.clone()),
-            server_tx: Some(server_tx.clone()),
-        })));
-        _ = service_manager.get_sender(1).send(msg);
+    //let mut client_tx: Arc<RwLock<Option<Sender<Box<SuperProtocol>>>>> =
+    //    Arc::new(RwLock::new(None));
 
-        // Invoke run so server and client can process messages
-        service_manager.run();
+    // This would be better shared via a message but this is "quick/dirty" for now.
+    let client_tx: Arc<RwLock<Option<SenderWrapper<Box<SuperProtocol>>>>> =
+        Arc::new(RwLock::new(None));
 
-        // Re-enable running, works for this simple test
-        service_manager.enable_running();
-    }
+    //for _ in 0..2 {
+
+        let client_thread_handle = thread::spawn(move || {
+            let client = Client::default();
+            let mut client_service_manager = ServiceManager::default();
+            client_service_manager.register_service(Box::new(client));
+
+            let c_tx = client_service_manager.get_sender(0);
+
+            let x = client_tx.clone();
+            let mut writer_lock_client_tx = x.write().unwrap();
+            let sw = SenderWrapper(c_tx);
+            *writer_lock_client_tx = Some(sw);
+
+            // Invoke run so client can process messages
+            client_service_manager.run();
+
+            client_service_manager.enable_running();
+        });
+
+        let server_thread_handle = thread::spawn(move || {
+            // Wait for client_tx to be Some
+            let c_tx_sw = loop {
+                let x = client_tx.clone();
+                let reader_lock_client_tx = x.read().unwrap();
+                if let Some(sw) = *reader_lock_client_tx {
+                    break sw
+                }
+            };
+
+            let server = Server::default();
+            let mut server_service_manager = ServiceManager::default();
+            server_service_manager.register_service(Box::new(server));
+
+            let s_tx = server_service_manager.get_sender(0);
+
+            let msg = Box::new(SuperProtocol::P2(Pinger::Start(StartMsg {
+                count: 1,
+                client_tx: Some((c_tx_sw.0).clone()),
+                server_tx: Some(s_tx.clone()),
+            })));
+            _ = server_service_manager.get_sender(0).send(msg);
+
+            // Invoke run so server can process messages
+            server_service_manager.run();
+
+            server_service_manager.enable_running();
+        });
+
+        client_thread_handle.join();
+        //server_thread_handle.join();
+    //}
 }
